d5ff2a7d914e3dd1749861e0bb72374fb86af763
nikolozb <berose995@gmail.com>
2023-01-05 13:54:31 +0400

    configured mongodb

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const DeviceRoutes = require("./routes/DeviceRoutes");
 const UserRoutes = require("./routes/UserRoutes");
 
+dotenv.config();
 const PORT = process.env.PORT || 5002;
+const DB_URI = process.env.DB_URI;
 const app = express();
 
 app.use(express.json());
@@ -11,4 +15,18 @@ app.use(cors());
 app.use("/users", UserRoutes);
 app.use("/devices", DeviceRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on: ${PORT}`));
+async function startServer() {
+  try {
+    await mongoose.set("strictQuery", false);
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    app.listen(PORT, () => console.log(`Server is running on: ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+startServer();
