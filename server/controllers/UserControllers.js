d1ec766ff8eec773344e43a3a652033831247134
nikolozb <berose995@gmail.com>
2023-01-05 13:34:20 +0400

    users routes with controllers

--- server/controllers/UserControllers.js
+++ server/controllers/UserControllers.js
@@ -1,3 +1,15 @@
-class UserControllers {}
+class UserControllers {
+  async getUsers(req, res) {
+    res.status(200).send("getUsers");
+  }
+
+  async createUser(req, res) {
+    res.status(200).send("getUsers");
+  }
+
+  async deleteUser(req, res) {
+    res.status(200).send("getUsers");
+  }
+}
 
 module.exports = new UserControllers();
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const UserControllers = require("../controllers/UserControllers");
+
+router.get("/", UserControllers.getUsers);
+router.post("/", UserControllers.createUser);
+router.delete("/", UserControllers.deleteUser);
 
 module.exports = router;
